82bd886acabcf6ffa5882b184a1b3d27d8e06c0d
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-22 09:52:01 +0000

    Jan 22, 2023, 1:22 PM

--- main.py
+++ main.py
@@ -10,15 +10,8 @@ def manhattan_distance(puzzle, goal):
                 distance += abs(i - x) + abs(j - y)
     return distance
 
-# Function to find the coordinates of the blank space
-def find_blank(puzzle):
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[0])):
-            if puzzle[i][j] == 0:
-                return (i, j)
-
 # Function to generate the possible next states
-def next_states(puzzle, blank):
+def next_states(puzzle, blank, goal):
     i, j = blank
     states = []
     if i > 0:
@@ -34,55 +27,7 @@ def next_states(puzzle, blank):
     if j > 0:
         new_puzzle = [row[:] for row in puzzle]
         blank_num = new_puzzle[i][j-1]
-        new_puzzle[i][j], new_puzzle[i][j-1] = new_puzzle[i][j-1], new_puzzle[i][j]
-        states.append((new_puzzle, 'left', blank_num))
-    if j < len(puzzle[0]) - 1:
-        new_puzzle = [row[:] for row in puzzle]
-        blank_num = new_puzzle[i][j+1]
-        new_puzzle[i][j], new_puzzle[i][j+1] = new_puzzle[i][j+1], new_puzzle[i][j]
-        states.append((new_puzzle, 'right', blank_num))
-    return states
-
-# A* search function
-def a_star(puzzle, goal):
-    # Initialize the heap with the starting state
-    heap = [(manhattan_distance(puzzle, goal), puzzle, [])]
-    # Create a set to keep track of the visited states
-    visited = set()
-    while heap:
-        # Pop the state with the lowest f(n) from the heap
-        _, current_puzzle, moves = heapq.heappop(heap)
-        # If the current state is the goal state, return the moves
-        if current_puzzle == goal:
-            return moves
-        # If the current state has already been visited, skip it
-        if str(current_puzzle) in visited:
-            continue
-        # Mark the current state as visited
-        visited.add(str(current_puzzle))
-        # Print the current state of the puzzle
-        for row in current_puzzle:
-            print(row)
-        # Find the coordinates of the blank space
-        blank = find_blank(current_puzzle)
-        # Generate the possible next states
-        for next_puzzle, move, blank_num in next_states(current_puzzle, blank):
-            # Append the move to the list of moves
-            next_moves = moves + [move]
-            # Print the tile number and arrow for the next move
-            print(f"{blank_num}-{move}")
-            if move == 'up':
-                print("↑")
-            elif move == 'down':
-                print("↓")
-            elif move == 'left':
-                print("←")
-            elif move == 'right':
-                print("→")
-            # Calculate the heuristic value
-            h = manhattan_distance(next_puzzle, goal)
-            # Add the next state to the heap with f(n) = g(n) + h(n)
-            heapq.heappush(heap, (len(next_moves) + h, next_puzzle, next_moves))
+        new_puzzle[i][j], new_puzzle[i][j-1] = new_puzzle[i][j-1], new_puzzle[i]
 
 # Example usage
 puzzle = [[1, 2, 3],
