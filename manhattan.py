49f3c1233381306989af22401f33e66ad5f78435
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-22 11:16:31 +0000

    Jan 22, 2023, 2:46 PM

--- manhattan.py
+++ manhattan.py
@@ -61,11 +61,13 @@ def a_star_search(puzzle, goal):
 def print_puzzle(puzzle, goal):
     puzzles = a_star_search(puzzle, goal)
     print("Manhattan distance:", manhattan_distance(puzzle))
-    for puzzle in puzzles:
-        print("Step:", puzzles.index(puzzle))
-        for row in puzzle:
-            print(row)
-        print("_____")
+    # if size=len(puzzle)^2 > 9, we need to adjust the width of the output
+    width = len(str(len(puzzle) ** 2))
+    for i in range(len(puzzles)):
+        print("Step", i)
+        for j in range(len(puzzles[i])):
+            print(" ".join([str(x).rjust(width) for x in puzzles[i][j]]))
+        print()
         
 
 # puzzle =  [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
