c4fdad09c13dad73f6b9ca54dca494dcd05ecffc
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-22 11:21:49 +0000

    Jan 22, 2023, 2:51 PM

--- manhattan.py
+++ manhattan.py
@@ -70,10 +70,10 @@ def print_puzzle(puzzle, goal):
         print()
         
 
-# puzzle =  [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
-# goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+puzzle =  [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
+goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
     
-# print_puzzle(puzzle, goal)
+print_puzzle(puzzle, goal)
 
 # let's try a harder puzzle
 # puzzle = [[1, 2, 3], [4, 6, 0], [7, 5, 8]]
@@ -82,4 +82,7 @@ def print_puzzle(puzzle, goal):
 # print_puzzle(puzzle, goal)
 
 # let's try a 4* 4easy puzzle(just one step)
-puzzle 
+puzzle = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
+goal = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+
+print_puzzle(puzzle, goal)
