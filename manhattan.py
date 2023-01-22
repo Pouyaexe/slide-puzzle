caa285deceaca8bf1ad8b563d683e6d33982f82c
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-22 10:45:57 +0000

    Jan 22, 2023, 2:15 PM

--- manhattan.py
+++ manhattan.py
@@ -38,22 +38,25 @@ def manhattan_distance(puzzle):
                 distance += abs(i - (puzzle[i][j] - 1) // 3) + abs(j - (puzzle[i][j] - 1) % 3)
     return distance
 
+import heapq # for priority queue implementation we use heapq module which is a min heap
+
 def a_star_search(puzzle, goal):
     frontier = []
     frontier.append(puzzle)
     explored = []
     while len(frontier) > 0:
-        frontier.sort(key = lambda x: manhattan_distance(x))
         current = frontier.pop(0)
-        explored.append(current)
         if current == goal:
             return explored
+        explored.append(current)
         neighbors = get_neighbors(current)
         for neighbor in neighbors:
             new_puzzle = move(current, neighbor)
             if new_puzzle not in explored and new_puzzle not in frontier:
                 frontier.append(new_puzzle)
-    return []
+                heapq.heapify(frontier) 
+                frontier.sort(key=lambda x: manhattan_distance(x))
+    return explored
 
 def print_puzzle(puzzle, goal):
     puzzles = a_star_search(puzzle, goal)
