c8da28482175e2731ba7d585a32c8cf1ec6bc213
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-26 11:36:27 +0000

    Jan 26, 2023, 3:06 PM

--- humming.py
+++ humming.py
@@ -56,7 +56,8 @@ def a_star_search(puzzle, goal):
     frontier.append(puzzle)
     explored = []
     while len(frontier) > 0:
-        frontier.sort(key = lambda x: hammingDistance(x, goal)) 
+        frontier.sort(key = lambda x: hammingDistance(x, goal)) # sort the frontier by the hamming distance. The puzzle with the lowest hamming distance will be the first in the frontier
+        # key = 
         current = frontier.pop(0)
         explored.append(current)
         if current == goal:
