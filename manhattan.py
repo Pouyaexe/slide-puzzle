d2676a58488e79d71b0c04f1081bde59dfaf70a5
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-26 12:25:43 +0000

    Jan 26, 2023, 3:55 PM

--- manhattan.py
+++ manhattan.py
@@ -118,4 +118,4 @@ print_puzzle(puzzle, goal)
 puzzle = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
 goal = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
 
-print_puzzle
+print_puzzle_set(puzzle, goal)
