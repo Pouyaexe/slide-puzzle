92c3a30463e167e46e069d205d42e3a62250d6a8
Pouya <pouyahallaj@gmail.com>
2023-01-28 13:02:56 +0000

    Jan 28, 2023, 4:32 PM

--- manhattan_set.py
+++ manhattan_set.py
@@ -45,6 +45,7 @@ def manhattan_distance(puzzle): # calculate the manhattan distance: h(n)= sum of
 
 def a_star_search(puzzle, goal):
     "Using set to implement priority queue for A* search"
+    puzzle = tuple(puzzle)
     frontier = set()
     frontier.add(puzzle)
     explored = set()
