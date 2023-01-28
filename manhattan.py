c7e860b9a06255579e3a27750ec8b279e5e5b403
Pouya <pouyahallaj@gmail.com>
2023-01-28 15:17:06 +0000

    Jan 28, 2023, 6:47 PM

--- manhattan.py
+++ manhattan.py
@@ -37,7 +37,7 @@ def manhattan_distance(puzzle): # calculate the manhattan distance: h(n)= sum of
     for i in range(len(puzzle)):
         for j in range(len(puzzle[0])): #abs means 
             distance += abs(i - (puzzle[i][j] - 1) // len(puzzle[0])) + abs(j - (puzzle[i][j] - 1) % len(puzzle[0]))
-            print(abs(i - (puzzle[i][j] - 1) // len(puzzle[0])) + abs(j - (puzzle[i][j] - 1) % len(puzzle[0])), )
+            print(abs(i - (puzzle[i][j] - 1) // len(puzzle[0])) + abs(j - (puzzle[i][j] - 1) % len(puzzle[0])), "::", puzzle[i][j] )
     return distance
 
 
