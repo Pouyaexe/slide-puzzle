0d6237d3c673449ca2784601c75f71e6d0d4fe75
Pouya <pouyahallaj@gmail.com>
2023-01-28 14:31:44 +0000

    Jan 28, 2023, 6:01 PM

--- humming.py
+++ humming.py
@@ -58,7 +58,7 @@ def a_star_search(puzzle, goal): # A* search algorithm
     explored = []
     while len(frontier) > 0:
         frontier.sort(key = lambda x: hammingDistance(x, goal)) # sort the frontier by the hamming distance. The puzzle with the lowest hamming distance will be the first in the frontier
-        current = frontier.pop(0)
+        current = frontier.pop(0) # 
         explored.append(current)
         if current == goal:
             return explored
