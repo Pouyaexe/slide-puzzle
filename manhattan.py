ba1aba58100f94cde861c050d978828c74726e2e
Pouya <pouyahallaj@gmail.com>
2023-01-28 15:37:04 +0000

    Jan 28, 2023, 7:07 PM

--- manhattan.py
+++ manhattan.py
@@ -77,7 +77,7 @@ def print_puzzle(puzzle, goal):
 puzzle =  [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
 goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
     
-print_puzzle(puzzle, goal)
+# print_puzzle(puzzle, goal)
 
 # let's try a harder puzzle
 # puzzle = [[1, 2, 3], [4, 6, 0], [7, 5, 8]]
@@ -89,3 +89,4 @@ print_puzzle(puzzle, goal)
 puzzle = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
 goal = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
 
+print_puzzle(puzzle, goal)
