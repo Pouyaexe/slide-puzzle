15bdf5f8061a6918d80f701bbbfdd35f213f9d71
Pouya <pouyahallaj@gmail.com>
2023-01-31 17:03:07 +0000

    Jan 31, 2023, 8:33 PM

--- manhattan_set.py
+++ manhattan_set.py
@@ -93,3 +93,4 @@ print_puzzle(puzzle, goal)
 # let's try a 4* 4easy puzzle(just one step)
 puzzle = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
 goal = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+
