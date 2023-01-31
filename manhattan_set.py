17c129e90028a2019f8d8a0c5d5ea6a2c8510e6e
Pouya <pouyahallaj@gmail.com>
2023-01-31 17:08:42 +0000

    Jan 31, 2023, 8:38 PM

--- manhattan_set.py
+++ manhattan_set.py
@@ -4,80 +4,94 @@
 # We will use hip in this example, but you can use any other heuristic function.
 
 
-import heapq  # for priority queue implementation we use
-
-
 def get_zero(puzzle):
+    """Get the position of the zero in the puzzle.
+    Args:
+        puzzle: the puzzle to be solved
+    Returns:
+        a list of lists
+    """
     for i in range(len(puzzle)):
         for j in range(len(puzzle[0])):
             if puzzle[i][j] == 0:
                 return [i, j]
+            
 
-
+# get the neighbors of the zero (up, down, left, right)
 def get_neighbors(puzzle):
+    """Get the neighbors of the zero in the puzzle.
+    Args:
+        puzzle: the puzzle to be solved
+        
+    Returns:
+        a list of lists of two integers, the row and column of the neighbors
+    """
     neighbors = []
     zero = get_zero(puzzle)
-    if zero[0] > 0:
+    if zero[0] > 0: # if zero is not in the first row because we can't move up
+        # zero = [i,j] /// zero[0] = i , zero[1] = j
         neighbors.append([zero[0] - 1, zero[1]])
     if zero[0] < len(puzzle) - 1:
         neighbors.append([zero[0] + 1, zero[1]])
-    if zero[1] > 0:
+    if zero[1] > 0: # if zero is not in the first column because we can't move left
         neighbors.append([zero[0], zero[1] - 1])
     if zero[1] < len(puzzle[0]) - 1:
         neighbors.append([zero[0], zero[1] + 1])
     return neighbors
 
-
-def move(puzzle, neighbor):
+def move(puzzle, neighbor): # move the zero to the neighbor position and return the new puzzle (sliding the zero)
     new_puzzle = [row[:] for row in puzzle]
     zero = get_zero(new_puzzle)
-    new_puzzle[zero[0]][zero[1]] = new_puzzle[neighbor[0]][neighbor[1]]
+    new_puzzle[zero[0]][zero[1]] = new_puzzle[neighbor[0]][neighbor[1]] 
     new_puzzle[neighbor[0]][neighbor[1]] = 0
     return new_puzzle
 
+def hammingDistance(puzle, goal): # calculate the number of tiles that are not in the right position (hamming distance): h(n)= number of misplaced tiles
+    ans = 0
+    for i in range(len(puzle)):
+        for j in range(len(puzle[0])):
+            if puzle[i][j] != goal[i][j]: # if the tile is not in the right position (not equal to the goal)
+                ans += 1
+    return ans           
 
-def manhattan_distance(
-    puzzle,
-):  # calculate the manhattan distance: h(n)= sum of the distances of the tiles from their goal positions
-    distance = 0
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[0])):
-            if puzzle[i][j] != 0:
-                distance += abs(i - (puzzle[i][j] - 1) // len(puzzle[0])) + abs(
-                    j - (puzzle[i][j] - 1) % len(puzzle[0])
-                )
-    return distance
-
-
-def a_star_search(puzzle, goal):
-    frontier = set()
-    puzzle = tuple(puzzle)
-    frontier.add(puzzle)
-    explored = set()
+def a_star_search(puzzle, goal): # A* search algorithm 
+    frontier = [] 
+    frontier.append(puzzle)
+    explored = []
     while len(frontier) > 0:
-        state = frontier.pop()
-        if tuple(state) == goal:
-            return explored + [state]
-        explored.add(tuple(state))
-        for neighbor in get_neighbors(state):
-            new_state = move(state, neighbor)
-            if tuple(new_state) not in explored and new_state not in frontier:
-                frontier.add(tuple(new_state))
-                heapq.heappush(frontier, (manhattan_distance(new_state), new_state))
-    return explored
-
+        frontier.sort(key = lambda x: hammingDistance(x, goal)) # sort the frontier by the hamming distance. The puzzle with the lowest hamming distance will be the first in the frontier
+        # print(frontier[0], "\n")
+        current = frontier.pop(0) #
+        explored.append(current) 
+        # print(current, "current")
+        # print(explored, "explored")
+        if current == goal:
+            return current
+        neighbors = get_neighbors(current)
+        for neighbor in neighbors:
+            new_puzzle = move(current, neighbor)
+            if new_puzzle not in explored:
+                frontier.append(new_puzzle) # add the new puzzle to the frontier
+    return None
 
 def print_puzzle(puzzle, goal):
-    puzzles = a_star_search(puzzle, goal)
-    print("Manhattan distance:", manhattan_distance(puzzle))
-    # if size=len(puzzle)^2 > 9, we need to adjust the width of the output
-    width = len(str(len(puzzle) ** 2))
-    for i in range(len(puzzles)):
-        print("Step", i)
-        for j in range(len(puzzles[i])):
-            print(" ".join([str(x).rjust(width) for x in puzzles[i][j]]))
-        print("_" * len(" ".join([str(x).rjust(width) for x in puzzles[i][j]])))
-        print()
+    """Print the puzzle and the steps to solve it.
+    Args:
+        puzzle: the puzzle to be solved
+        goal: the goal state of the puzzle
+    """
+    print("Puzzle:")
+    for row in puzzle:
+        print(row)
+    print("Goal:")
+    for row in goal:
+        print(row)
+    print("Steps:")
+    steps = a_star_search(puzzle, goal)
+    for step in steps:
+        print(step)
+    print("")
+        
 
 
 puzzle = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
